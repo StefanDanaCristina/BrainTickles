28a5ae93ebe2efe820a9cf56f2a2633ce95afe34
Cristina Stefan <cstefan@murex.com>
2023-10-17 16:11:56 +0300

    arrayContainsDuplicates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
+        System.out.println(arrayContainsDuplicates());
+    }
+    public static boolean arrayContainsDuplicates(){
+        Integer[] nums = {7,2,3,4,3};
+        Set uniqueList = new HashSet();
+        for (int num:nums
+             ) {
+            if (uniqueList.contains(num)) return true;
+            uniqueList.add(num);
+        }
+        return false;
     }
 }
